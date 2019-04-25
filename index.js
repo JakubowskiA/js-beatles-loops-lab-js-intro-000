e5d20dcedbd50e0a703ef5624001ab31de8fd4fa
Ariel J <arieljakubowski@gmail.com>
2019-04-25 17:53:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+let beatlesStr;
+let beatlesInstrument=[];
+for( let i = 0; i<musicians.length; i++){
+  beatlesStr = musicians[i] + ' plays ' + instruments[i];
+  beatlesInstrument[i] = beatlesStr;
+}
+return beatlesInstrument;
+}
+
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+function johnLennonFacts(facts) {
+
+let newFacts = [];
+let i = 0;
+i = i + 1;
+while(i<facts.length)  {
+  newFacts.push(facts[i] + '!!!');
+}
+return newFacts;
+}
